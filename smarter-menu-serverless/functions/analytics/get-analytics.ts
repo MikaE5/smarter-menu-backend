ceab971397320ab9b5ec17b4cf71805868636148
MikaE5 <mika.eckhardt@gmx.de>
2021-10-06 21:13:34 +0200

    fix handler of get analytics

--- smarter-menu-serverless/functions/analytics/get-analytics.ts
+++ smarter-menu-serverless/functions/analytics/get-analytics.ts
@@ -1,13 +1,11 @@
+import middy from '@middy/core';
+import httpErrorHandler from '@middy/http-error-handler';
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import { getDatabase } from '../../database/database';
-import {
-  getAllTypeForCustomerQuery,
-  getAllWishlistsForCustomerQuery,
-} from '../../database/util/database.util';
+import { getAllWishlistsForCustomerQuery } from '../../database/util/database.util';
 import { getBody } from '../../util/api-request.util';
 import { apiResponse } from '../../util/api-response.util';
 import { isThisMonth, isThisWeek, isToday } from '../../util/date.util';
-import { Document } from '../model/document.interface';
 import { verifyToken } from '../util/auth.util';
 import { isDefinedString } from '../util/validate-body.util';
 import {
@@ -16,97 +14,94 @@ import {
   WishlistEntry,
 } from './model/wishlist.interface';
 
-export const baseGetAll = (
-  type: string,
-  filterDisabled = false
-): ((event: APIGatewayProxyEvent) => Promise<APIGatewayProxyResult>) => {
-  return async (
-    event: APIGatewayProxyEvent
-  ): Promise<APIGatewayProxyResult> => {
-    const body = getBody(event.body);
+const getAnalytics = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  const body = getBody(event.body);
 
-    const token: string = body['token'];
-    const customerId = body['customer_id'];
+  const token: string = body['token'];
+  //const customerId = body['customer_id'];
 
-    if (!isDefinedString(token) || !isDefinedString(customerId)) {
-      return apiResponse._400();
-    }
+  if (!isDefinedString(token)) {
+    return apiResponse._400();
+  }
 
-    const { valid, customer } = verifyToken(token);
+  const { valid, customer } = verifyToken(token);
 
-    if (!valid || customer === undefined) {
-      return apiResponse._401();
-    }
+  if (!valid || customer === undefined) {
+    return apiResponse._401();
+  }
 
-    try {
-      const items = await getDatabase()
-        .query(getAllWishlistsForCustomerQuery(customerId))
-        .promise();
+  try {
+    const items = await getDatabase()
+      .query(getAllWishlistsForCustomerQuery(customer))
+      .promise();
 
-      const wishlists: Wishlist[] = items.Items
-        ? (items.Items as Wishlist[])
-        : [];
+    const wishlists: Wishlist[] = items.Items
+      ? (items.Items as Wishlist[])
+      : [];
 
-      const today = new Date();
+    const today = new Date();
 
-      const itemAnalyticsMap: {
-        [itemId: string]: { [categoryId: string]: WishlistAnalyticsEntry };
-      } = {};
+    const itemAnalyticsMap: {
+      [itemId: string]: { [categoryId: string]: WishlistAnalyticsEntry };
+    } = {};
 
-      const addToMap = (
-        wishlistEntry: WishlistEntry,
-        wishlistEntryDate: Date
-      ) => {
-        const addAnalyticsEntry: WishlistAnalyticsEntry = {
-          item_id: wishlistEntry.item_id,
-          category_id: wishlistEntry.category_id,
-          all_time: 1,
-          this_month: isThisMonth(today, wishlistEntryDate) ? 1 : 0,
-          this_week: isThisWeek(today, wishlistEntryDate) ? 1 : 0,
-          today: isToday(today, wishlistEntryDate) ? 1 : 0,
-        };
-
-        if (
-          itemAnalyticsMap[wishlistEntry.item_id] === undefined ||
-          itemAnalyticsMap[wishlistEntry.item_id][wishlistEntry.category_id] ===
-            undefined
-        ) {
-          itemAnalyticsMap[wishlistEntry.item_id] = {
-            [wishlistEntry.category_id]: addAnalyticsEntry,
-          };
-          return;
-        }
-
-        const mapEntry =
-          itemAnalyticsMap[wishlistEntry.item_id][wishlistEntry.category_id];
-
-        mapEntry.all_time += addAnalyticsEntry.all_time;
-        mapEntry.this_month += addAnalyticsEntry.this_month;
-        mapEntry.this_week += addAnalyticsEntry.this_week;
-        mapEntry.today += addAnalyticsEntry.today;
+    const addToMap = (
+      wishlistEntry: WishlistEntry,
+      wishlistEntryDate: Date
+    ) => {
+      const addAnalyticsEntry: WishlistAnalyticsEntry = {
+        item_id: wishlistEntry.item_id,
+        category_id: wishlistEntry.category_id,
+        all_time: 1,
+        this_month: isThisMonth(today, wishlistEntryDate) ? 1 : 0,
+        this_week: isThisWeek(today, wishlistEntryDate) ? 1 : 0,
+        today: isToday(today, wishlistEntryDate) ? 1 : 0,
       };
 
-      wishlists.forEach((wishlist: Wishlist) => {
-        const wishlistDate = new Date(wishlist.created_at);
-        wishlist.wishlist.forEach((entry: WishlistEntry) =>
-          addToMap(entry, wishlistDate)
-        );
-      });
+      if (
+        itemAnalyticsMap[wishlistEntry.item_id] === undefined ||
+        itemAnalyticsMap[wishlistEntry.item_id][wishlistEntry.category_id] ===
+          undefined
+      ) {
+        itemAnalyticsMap[wishlistEntry.item_id] = {
+          [wishlistEntry.category_id]: addAnalyticsEntry,
+        };
+        return;
+      }
 
-      const categoryMaps: Array<{
-        [categoryId: string]: WishlistAnalyticsEntry;
-      }> = Object.values(itemAnalyticsMap);
-      const wishlistAnalytics: WishlistAnalyticsEntry[] = [];
-      categoryMaps.forEach((categoryMap) => {
-        wishlistAnalytics.push(...Object.values(categoryMap));
-      });
+      const mapEntry =
+        itemAnalyticsMap[wishlistEntry.item_id][wishlistEntry.category_id];
 
-      return apiResponse._200({
-        data: wishlistAnalytics,
-      });
-    } catch (error) {
-      console.log(error);
-      return apiResponse._500();
-    }
-  };
+      mapEntry.all_time += addAnalyticsEntry.all_time;
+      mapEntry.this_month += addAnalyticsEntry.this_month;
+      mapEntry.this_week += addAnalyticsEntry.this_week;
+      mapEntry.today += addAnalyticsEntry.today;
+    };
+
+    wishlists.forEach((wishlist: Wishlist) => {
+      const wishlistDate = new Date(wishlist.created_at);
+      wishlist.wishlist.forEach((entry: WishlistEntry) =>
+        addToMap(entry, wishlistDate)
+      );
+    });
+
+    const categoryMaps: Array<{
+      [categoryId: string]: WishlistAnalyticsEntry;
+    }> = Object.values(itemAnalyticsMap);
+    const wishlistAnalytics: WishlistAnalyticsEntry[] = [];
+    categoryMaps.forEach((categoryMap) => {
+      wishlistAnalytics.push(...Object.values(categoryMap));
+    });
+
+    return apiResponse._200({
+      data: wishlistAnalytics,
+    });
+  } catch (error) {
+    console.log(error);
+    return apiResponse._500();
+  }
 };
+
+export const handler = middy(getAnalytics).use(httpErrorHandler());
